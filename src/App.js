2718ffba5d105d0f06511fc01cb8defe3692b60d
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2023-08-17 15:33:27 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
+import AllocationForm from './components/AllocationForm';
 import Budget from './components/Budget';
-
-// Add code to import the other components here under
-
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList'; 
+import ExpenseTotal from './components/ExpenseTotal'; 
+import Remaining from './components/Remaining'; 
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -15,18 +17,9 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
-
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                 </div>
             </div>
         </AppProvider>
